7f0ebd9ae4cd40581a87dcb0db51371adaad689d
zhangguoyong <gary@garymacpro.local>
2017-08-25 18:26:18 +0800

    add log

--- src/main/java/com.moli.admin/controller/demoController.java
+++ src/main/java/com.moli.admin/controller/demoController.java
@@ -1,6 +1,8 @@
 package com.moli.admin.controller;
 
 import com.moli.admin.model.HelloMesage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,11 @@ import javax.validation.Valid;
 @RequestMapping("/demo")
 public class demoController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(demoController.class);
+
     @GetMapping("{userName}/say")
     public String sayHello(@Valid HelloMesage helloMesage, @PathVariable("userName") String userName){
+        LOGGER.info(">>>>>>>>my logger");
         return userName + " " + helloMesage.getName() + " " + helloMesage.getMessage();
     }
 
