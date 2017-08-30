5ddcff1ec73c98353d0454724059d5c5af0e376d
zhangguoyong <gary@garymacpro.local>
2017-08-30 22:52:55 +0800

    update

--- src/main/java/com.moli.admin/utils/FileUtil.java
+++ src/main/java/com.moli.admin/utils/FileUtil.java
@@ -3,8 +3,6 @@ package com.moli.admin.utils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by gary on 17/8/26.
@@ -13,34 +11,34 @@ public class FileUtil {
     //private static final String ss = "8613082439536,8613188628530,8613052784059,8613032485392,8613190453060,8613130006541,8613147852341,8613002403945,8613080717776,8613066696371,8613190024947,8613042462796,8613124271191,8613188285207,8618512493156,8613080899663,8613081307468,8613130201811,8613052628086,8613274224613,8613164533233,8618640412548,8613166799091,8613236962699,8615641287789,8613032474150,8613020317315,8613166723998,8613008213609,8615642915136,8613081303720,8613125581285,8618641337873,8613066596420,8613019887677,8613284200809,8618624621341,8613066673450,8613065394026,8613052679911,8613130235542,8615541289235,8613130250537,8613019848907,8613234067840,8613009342969,8615541255403,8613234066747,8613009307198,8613236966844,8613029431599,8613130036468,8613050381620,8613204103587,8613029240878,8613032487817,8613009263163,8613188483426,8613130891215,8613052626764,8613194141175,8613190127310,8613204202139,8613224281126,8613134240407,8613079891771,8615642136734,8613104248802,8613188255231,8613080859827,8613052621541,8613194299697,8613238938949,8613166712471,8613065355775,8613130839135,8613224204549,8613042485205,8615566257713,8613022454621,8613130969731,8613124277750,8613190113126,8613082491552,8613284231755,8618540017127,8613008256644,8613125624000,8613238038273,8613124235049,8613274106168,8613080828237,8615642113576,8615524413412,8613166797112,8613130407551,8613190032581,8613188473037,8613050872848,8613050416449,8613194209968,8613019675589,8613130663768,8615641359942,8613190089671,8618641321945,8615566539356,8615542470299,8613252737051,8613252737053,8618641243663,8618641944430,8615566131573,8613130324173,8613072437045,8613234174642,8615541304447,8618512419261,8618640349366,8613109867942,8613234077674,8613020327526,8613238802911,8613124238430,8613050940976,8613252790781,8613082222874,8613065303987,8613074161972,8613066579949,8618641169030,8613238890241,8613019331240,8615604059223,8613104227695,8613238938517,8615641464587,8613019919917,8617612498873,8613125507385,8613130218321,8613274201856,8615541253285,8613130567913,8613190340846,8615542124647,8618524731912,8613125647075,8613028202697,8613238893876,8615641212529,8613149771739,8613236757159,8613104220691,8613188077052,8613188459626,8613082416487,8613284212697,8613234117419,8613284162729,8615640768212,8613020328062,8613065226168,8615641201656";
 
     public static void main(String[] args) {
-//        String filePaht = "/Users/gary/Desktop/FTP文件夹";
-//        File file = new File(filePaht);
-//        if (file.isDirectory()) {
-//            String[] fileList = file.list();
-//            System.out.println(fileList.length);
-//            String outFilePaht = "/Users/gary/Desktop/result.txt";
-//            File inFile;
-//            String fileName;
-//            String content;
-//            for (int i = 0; i<fileList.length; i++) {
-//                if (!fileList[i].contains(".req")) continue;
-//                fileName = filePaht + "/" + fileList[i];
-//                System.out.println("file count is : " + i + ", file name is " + fileName);
-//                inFile = new File(fileName);
-//                if (inFile.isFile()) {
-//                    content = readFileByLines(inFile, null);
-//                    if (StringUtils.isBlank(content)) continue;
-//                    writeFile(outFilePaht, content);
-//                }
-//            }
-//        }
-        Map map = new HashMap();
-        map.put("233", 233);
-        System.out.println(map.get("233"));
-
-        map.put("233", 234434);
-
-        System.out.println(map.get("233"));
+        String filePaht = "/Users/gary/Desktop/FTP文件夹";
+        File file = new File(filePaht);
+        if (file.isDirectory()) {
+            String[] fileList = file.list();
+            System.out.println(">>>>>> "+fileList.length);
+            String outFilePaht = "/Users/gary/Desktop/result_ftp.txt";
+            File inFile;
+            String fileName;
+            String content;
+            for (int i = 0; i<fileList.length; i++) {
+                if (!fileList[i].contains(".req")) continue;
+                fileName = filePaht + "/" + fileList[i];
+                System.out.println("file count is : " + i + ", file name is " + fileName);
+                inFile = new File(fileName);
+                if (inFile.isFile()) {
+                    content = readFileByLines(inFile, null);
+                    if (StringUtils.isBlank(content)) continue;
+                    writeFile(outFilePaht, content);
+                }
+            }
+        }
+//        Map map = new HashMap();
+//        map.put("233", 233);
+//        System.out.println(map.get("233"));
+//
+//        map.put("233", 234434);
+//
+//        System.out.println(map.get("233"));
     }
 
     /**
--- src/main/java/com.moli.admin/web/rest/MoliOrderController.java
+++ src/main/java/com.moli.admin/web/rest/MoliOrderController.java
@@ -1,7 +1,6 @@
 package com.moli.admin.web.rest;
 
 import com.moli.admin.common.Result;
-import com.moli.admin.domain.moli.SmsOrderUnicom;
 import com.moli.admin.service.MoliOrderService;
 import com.moli.admin.utils.FileUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -31,26 +30,28 @@ public class MoliOrderController {
     @Autowired
     private MoliOrderService moliOrderService;
 
-    private static final String ssss = "8615641001015,8618641034030,8615640060567,8618640394981,8613019359835,8618640444401,8613130229403,8615524511279,8613190239209,8613050320132,8618525532579,8615541170917,8618512416201,8615641285666,8618642162950,8618641201205,8613050421116,8618640121288,8618604158896,8613009278904,8613042436323,8613284193951,8618525440017,8615524589068,8613050812863,8613188483453,8615641693433,8618540140802,8618624051656,8618624493279,8618641092549,8618640996058,8615541146171,8613164526237,8613050812863,8615566230226,8617616663725,8613042650542,8615609845509,8615641409819,8613166764278,8613066559310,8613214259405,8615542103651,8613050908420,8615641366662,8613236628219,8615541651899,8615541430129,8615566875958,8613134198880,8615524104417,8613188337687,8618641605000,8615668976180,8618642663266,8618624425167,8615566194523,8613066527355,8613166776899,8613050218810,8613149841542,8615566055633,8615604272898,8613124103299,8613028244997,8613019310339,8613084175777,8613104257556,8613190024321,8613072417781,8613234036922,8613252796805,8617640210827,8613188293719,8615640456127,8615640643678,8615640123855,8615642156934,8613030781108,8617615000104,8615541431499,8615542302070,8613284185006,8615542302070,8613080760113,8615502416011,8613204222850,8613042665100,8613130829119,8615524402410,8615641331529,8615668912654,8615541627583,8615640595029,8615694228969,8615541342380,8615541599631,8613188552522,8615642898444,8618524509510,8613081700209,8613188017343,8615566054311,8615566050203,8613147822339,8618524721680,8615542465250,8613166613099,8618540036408,8613166771289,8615566564844,8615542190302,8613050405242,8615542993379,8615698877285,8613065440116,8617604146935,8613082269969,8613188080644,8613050832530,8613009369932,8615541244712,8613081280206,8613020360329,8613130419999,8618502450709,8615541300477,8613154219815,8617640094492,8615668973079,8615566048530,8613109871156,8613188311332,8615542703095,8615542143045,8618524464410,8613050398575,8613130575909,8613166601314,8613188648116,8613149851121,8615524943832,8615668590098,8615640643621,8613164547674,8618524639520,8615566377858,8615524254994,8615542122500,8617640212063,8613066521432,8613190328908,8617640640851,8613188373843,8613052793234,8618524669794,8618504261798,8613008200895,8613125411261,8615541512567,8615566082169,8613130829399,8613234023316,8617641520316,8613238296083,8615642439496,8613009390627,8613081201537,8615524485589,8618640396788";
+    private static final String ssss = "18624463545,13252737051,13252737053,18512419261,13074161972,13019331240,15604059223,15640331541,18609887376,18624172170,18624588071,13104266631,13065320898,13029281802,18604126959,13130343441,13032463036,13188523538,13236780107,13079206818,13236881083,15524638126,13109867942,15542470299,15641464587,18641243663,18641169030,13238890241,18641944430,17640115432,18602415119";
 
     @GetMapping("do")
     public Result execute(){
         LOGGER.info(">>>>>>>>my logger");
-        findDiff();
+        String filePaht = "/Users/gary/Desktop/result_ftp.txt";
+        String outFilePath = "/Users/gary/Desktop/result_ftp_tx.txt";
+        findDiff(filePaht, outFilePath);
+
+        String filePaht1 = "/Users/gary/Desktop/result_logs.txt";
+        String outFilePath1 = "/Users/gary/Desktop/result_log_tx.txt";
+        findDiff(filePaht1, outFilePath1);
         return Result.SUCCESS;
     }
 
 
-    private void findDiff(){
+    private void findDiff(String filePaht, String outFilePath){
 
-        //String filePaht = "/Users/gary/Desktop/result.txt";
-        String fileLogPaht = "/Users/gary/Desktop/result_logs.txt";
-        //File inFile = new File(filePaht);
-        File inFile = new File(fileLogPaht);
-        String outFilePath = "/Users/gary/Desktop/result_final.txt";
-        String outFilenNullPath = "/Users/gary/Desktop/result_final_null.txt";
-        String outFilenNotTDPath = "/Users/gary/Desktop/result_final_notTD.txt";
-        String outFileConPath = "/Users/gary/Desktop/result_final_con.txt";
+        //String fileLogPaht = "/Users/gary/Desktop/result_logs.txt";
+        File inFile = new File(filePaht);
+        //File inFile = new File(fileLogPaht);
+        //String outFilenNullPath = "/Users/gary/Desktop/result_ftp_final_null.txt";
         Map<String, String> map = new HashMap<>();
         if (inFile.isFile()) {
             BufferedReader reader = null;
@@ -62,24 +63,46 @@ public class MoliOrderController {
                 // 一次读入一行，直到读入null为文件结束
                 while ((tempString = reader.readLine()) != null) {
                     // 显示行号
-                    System.out.println("line " + line + ": " + tempString);
+                    //System.out.println("line " + line + ": " + tempString);
                     line++;
                     if (StringUtils.isBlank(tempString)) continue;
-                    //String[] ss = tempString.split("\t");
-                    String[] ss = tempString.split(",");
+                    String[] ss = tempString.split("\t");
+                    //String[] ss = tempString.split(",");
+                    String se = ss[0].trim();
                     String mobile = ss[2].trim().substring(2,13);
+                    //String mobile = ss[2].trim();
                     String con = ss[10].trim();
-                    SmsOrderUnicom order = moliOrderService.queryByMobile(mobile);
+                    //System.out.println(">>>>>> " + mobile);
+//                    String tdCon = "";
+//                    if (ss.length >= 15) {
+//                        tdCon = ss[14].trim();
+//                    }
+//                    SmsOrderUnicom order = moliOrderService.queryByMobile(mobile);
                     StringBuffer sb = new StringBuffer();
-                    sb.append(tempString).append("\n");
+                    sb.append(tempString);
 //                    if (null == order) {
 ////                        if (!con.startsWith("TD")) {
 ////                            FileUtil.writeFile(outFilenNotTDPaht, sb.toString());
 ////                        }
-//                        FileUtil.writeFile(outFilenNullPath, sb.toString());
+//                        //FileUtil.writeFile(outFilenNullPath, sb.toString());
 //                        //map.put(mobile, sb.toString());
 //                        continue;
 //                    }
+//                    if (con.toLowerCase().startsWith("td")) {
+//                        Date time = order.getUnsubscribeTime();
+//                        if (null == time || !order.getContent().equals(tdCon)) {
+//                            map.put(mobile, sb.append("\n").toString());
+//                        }
+////                        String ssssss = map.get(mobile);
+////                        if (StringUtils.isNoneBlank(ssssss)) {
+////                            String content = ssssss.split("\t")[10];
+////                            sb.append(" ").append(content);
+////                        }
+//                    } else if (!order.getContent().equals(con)){
+//                        map.put(mobile, sb.append("\n").toString());
+//                    }
+                    //map.put(mobile, sb.append("\n").toString());
+
 //                    if (!order.getContent().trim().equals(con)){
 ////                        FileUtil.writeFile(outFilePaht, sb.toString());
 //                        map.put(mobile, sb.toString());
@@ -88,7 +111,8 @@ public class MoliOrderController {
 //                    }
                     if (ssss.contains(mobile)) {
                         line22++;
-                        FileUtil.writeFile(outFileConPath, sb.toString());
+//                        map.put(mobile, sb.toString());
+                        FileUtil.writeFile(outFilePath, sb.append("\n").toString());
                     }
                 }
                 System.out.println(">>>>>>line 22 is " + line22);
