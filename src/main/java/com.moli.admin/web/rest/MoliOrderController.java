69909bfbaf9ef2ee31ceedf7dd3b8345f7cc8e5e
zhangguoyong <gary@garymacpro.local>
2017-09-14 17:31:16 +0800

    udpate

--- src/main/java/com.moli.admin/web/rest/MoliOrderController.java
+++ src/main/java/com.moli.admin/web/rest/MoliOrderController.java
@@ -1,6 +1,7 @@
 package com.moli.admin.web.rest;
 
 import com.moli.admin.common.Result;
+import com.moli.admin.domain.moli.SmsOrderUnicom;
 import com.moli.admin.service.MoliOrderService;
 import com.moli.admin.utils.FileUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -30,18 +31,18 @@ public class MoliOrderController {
     @Autowired
     private MoliOrderService moliOrderService;
 
-    private static final String ssss = "18624463545,13252737051,13252737053,18512419261,13074161972,13019331240,15604059223,15640331541,18609887376,18624172170,18624588071,13104266631,13065320898,13029281802,18604126959,13130343441,13032463036,13188523538,13236780107,13079206818,13236881083,15524638126,13109867942,15542470299,15641464587,18641243663,18641169030,13238890241,18641944430,17640115432,18602415119";
+    //private static final String ssss = "18624463545,13252737051,13252737053,18512419261,13074161972,13019331240,15604059223,15640331541,18609887376,18624172170,18624588071,13104266631,13065320898,13029281802,18604126959,13130343441,13032463036,13188523538,13236780107,13079206818,13236881083,15524638126,13109867942,15542470299,15641464587,18641243663,18641169030,13238890241,18641944430,17640115432,18602415119";
 
     @GetMapping("do")
     public Result execute(){
         LOGGER.info(">>>>>>>>my logger");
-        String filePaht = "/Users/gary/Desktop/result_ftp.txt";
-        String outFilePath = "/Users/gary/Desktop/result_ftp_tx.txt";
+        String filePaht = "/Users/gary/Desktop/710logs.txt";
+        String outFilePath = "/Users/gary/Desktop/result_logs.txt";
         findDiff(filePaht, outFilePath);
 
-        String filePaht1 = "/Users/gary/Desktop/result_logs.txt";
-        String outFilePath1 = "/Users/gary/Desktop/result_log_tx.txt";
-        findDiff(filePaht1, outFilePath1);
+//        String filePaht1 = "/Users/gary/Desktop/result_logs.txt";
+//        String outFilePath1 = "/Users/gary/Desktop/result_log_tx.txt";
+//        findDiff(filePaht1, outFilePath1);
         return Result.SUCCESS;
     }
 
@@ -77,17 +78,17 @@ public class MoliOrderController {
 //                    if (ss.length >= 15) {
 //                        tdCon = ss[14].trim();
 //                    }
-//                    SmsOrderUnicom order = moliOrderService.queryByMobile(mobile);
+                    SmsOrderUnicom order = moliOrderService.queryByMobile(mobile);
                     StringBuffer sb = new StringBuffer();
                     sb.append(tempString);
-//                    if (null == order) {
-////                        if (!con.startsWith("TD")) {
-////                            FileUtil.writeFile(outFilenNotTDPaht, sb.toString());
-////                        }
-//                        //FileUtil.writeFile(outFilenNullPath, sb.toString());
-//                        //map.put(mobile, sb.toString());
-//                        continue;
-//                    }
+                    if (null == order) {
+//                        if (!con.startsWith("TD")) {
+//                            FileUtil.writeFile(outFilenNotTDPaht, sb.toString());
+//                        }
+                        FileUtil.writeFile(outFilePath, sb.append("\n").toString());
+                        //map.put(mobile, sb.toString());
+                        continue;
+                    }
 //                    if (con.toLowerCase().startsWith("td")) {
 //                        Date time = order.getUnsubscribeTime();
 //                        if (null == time || !order.getContent().equals(tdCon)) {
@@ -109,11 +110,11 @@ public class MoliOrderController {
 //                    } else {
 //                        map.remove(mobile);
 //                    }
-                    if (ssss.contains(mobile)) {
-                        line22++;
-//                        map.put(mobile, sb.toString());
-                        FileUtil.writeFile(outFilePath, sb.append("\n").toString());
-                    }
+//                    if (ssss.contains(mobile)) {
+//                        line22++;
+////                        map.put(mobile, sb.toString());
+//                        FileUtil.writeFile(outFilePath, sb.append("\n").toString());
+//                    }
                 }
                 System.out.println(">>>>>>line 22 is " + line22);
                 System.out.println(">>>>>>line count is " + line);
